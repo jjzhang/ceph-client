44060432d1940669be231c7ee69c5c4ab40bff96
Alex Elder <elder@inktank.com>
2012-05-29 21:47:39 -0500

    libceph: set CONNECTING state even earlier
    Move the setting of the CONNECTING state in a ceph connection
    all the way back to where a connection first gets opened.  At
    that point the connection's socket pointer is still null, and
    the connection sequence is about to begin.
    
    Signed-off-by: Alex Elder <elder@inktank.com>

--- net/ceph/messenger.c
+++ net/ceph/messenger.c
@@ -533,6 +533,7 @@ void ceph_con_open(struct ceph_connection *con, struct ceph_entity_addr *addr)
 	dout("con_open %p %s\n", con, ceph_pr_addr(&addr->in_addr));
 	set_bit(OPENING, &con->state);
 	WARN_ON(!test_and_clear_bit(CLOSED, &con->state));
+	set_bit(CONNECTING, &con->state);
 
 	memcpy(&con->peer_addr, addr, sizeof(*addr));
 	con->delay = 0;      /* reset backoff memory */
@@ -1981,8 +1982,6 @@ static void process_message(struct ceph_connection *con)
  */
 static int ceph_con_connect(struct ceph_connection *con)
 {
-	set_bit(CONNECTING, &con->state);
-
 	con_out_kvec_reset(con);
 	prepare_write_banner(con);
 	prepare_read_banner(con);
